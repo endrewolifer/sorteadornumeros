5833688b58b8fcf8a626bf23c8d303f439654daa
Endrew Fernandes <endrewoliveirafernandes@gmail.com>
2024-07-08 19:09:51 -0300

    Criar funcoes para sorteio

---
+++ app.js
@@ -0,0 +1,5 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+}
